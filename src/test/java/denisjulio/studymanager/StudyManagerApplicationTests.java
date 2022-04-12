face9977835a330296d2c04cc7ae78ba3463272c
Denis Julio <djms1234@gmail.com>
2022-04-12 15:49:03 -0300

    created spring-boot project structure

---
+++ src/test/java/denisjulio/studymanager/StudyManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package denisjulio.studymanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
